d4c41d501b9e33071ad06a0cc127e6abf0ab4946
Joel Edward <joledwrd@gmail.com>
2023-12-23 23:11:40 +0700

    vehicle done

---
+++ database/migrations/2023_12_14_144636_add_foreign_key_to_vehicles_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->foreign('vehicle_category_id')->references('id')->on('vehicle_categories')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('vehicles', function (Blueprint $table) {
+            $table->dropForeign('vehicles_vehicle_category_id_foreign');
+            $table->dropColumn('vehicle_category_id');
+        });
+    }
+};
